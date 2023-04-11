7de3bd8f4066326e39f8a8d96939fbd050e33257
Arman Aminian <arman.aminian78@gmail.com>
2023-04-12 01:21:48 +0330

    feat: category added to posts table.

--- src/collect_data/collect_data.py
+++ src/collect_data/collect_data.py
@@ -15,14 +15,15 @@ def transfer_csv_to_postgresql(csv_path_list, selected_columns=None):
 
     sql = '''CREATE TABLE POSTS(model int,\
     description varchar(1000),\
-    price varchar(50);'''
+    price varchar(50),\
+    category varchar(50);'''
     cur.execute(sql)
 
     for csv_path in csv_path_list:
         csv_file = pd.read_csv(csv_path)
         for i, row in csv_file.iterrows():
-            cur.execute(f'''INSERT INTO POSTS(model, description, price) VALUES ( 
-            {row['Model']}, {row['Description']}, {row['Price']})''')
+            cur.execute(f'''INSERT INTO POSTS(model, description, price, category) VALUES ( 
+            {row['Model']}, {row['Description']}, {row['Price']}, {csv_path.split('/')[-1]})''')
 
     conn.commit()
     conn.close()
