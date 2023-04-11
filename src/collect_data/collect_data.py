b2707b2b1ee98eb03b6441fed12d2b417e1a5754
Arman Aminian <arman.aminian78@gmail.com>
2023-04-12 01:53:06 +0330

    fix: insert to table bug fixed.

--- src/collect_data/collect_data.py
+++ src/collect_data/collect_data.py
@@ -16,14 +16,17 @@ def transfer_csv_to_postgresql(csv_path_list, selected_columns=None):
     sql = '''CREATE TABLE POSTS(model int,\
     description varchar(1000),\
     price varchar(50),\
-    category varchar(50);'''
+    category varchar(50));'''
     cur.execute(sql)
 
     for csv_path in csv_path_list:
         csv_file = pd.read_csv(csv_path)
         for i, row in csv_file.iterrows():
-            cur.execute(f'''INSERT INTO POSTS(model, description, price, category) VALUES ( 
-            {row['Model']}, {row['Description']}, {row['Price']}, {csv_path.split('/')[-1]})''')
+            desc = row['Description'].replace('\n', ' ').replace('"', "'")[:1000]
+            cur.execute(
+                'INSERT INTO POSTS (model, description, price, category) VALUES (%s, %s, %s, %s)',
+                (row['Model'], desc, row['Price'], csv_path.split('/')[-1], )
+            )
 
     conn.commit()
     conn.close()
