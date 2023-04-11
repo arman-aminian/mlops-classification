e98edb95ec8afb09465e1ea019f370cba5390b44
Arman Aminian <arman.aminian78@gmail.com>
2023-04-12 01:14:14 +0330

    posts table added.

--- src/collect_data/collect_data.py
+++ src/collect_data/collect_data.py
@@ -2,7 +2,7 @@ import psycopg2
 import pandas as pd
 
 
-def transfer_csv_to_postgresql(csv_path_list, selected_columns):
+def transfer_csv_to_postgresql(csv_path_list, selected_columns=None):
     conn = psycopg2.connect(
         database="exampledb",
         user="docker",
@@ -13,18 +13,16 @@ def transfer_csv_to_postgresql(csv_path_list, selected_columns):
 
     cur.execute("DROP TABLE IF EXISTS POSTS")
 
-    sql = '''CREATE TABLE POSTS(model int NOT NULL,\
+    sql = '''CREATE TABLE POSTS(model int,\
     description varchar(1000),\
     price varchar(50);'''
     cur.execute(sql)
 
-    cur.execute('''INSERT INTO POSTS(employee_id, employee_name, employee_email, employee_salary) VALUES ( 
-    3, 'nnn', 'eee', 9000)''')
-
     for csv_path in csv_path_list:
         csv_file = pd.read_csv(csv_path)
         for i, row in csv_file.iterrows():
-
+            cur.execute(f'''INSERT INTO POSTS(model, description, price) VALUES ( 
+            {row['Model']}, {row['Description']}, {row['Price']})''')
 
     conn.commit()
     conn.close()
