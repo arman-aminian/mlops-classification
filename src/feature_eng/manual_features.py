c718d3f01d6ac607f6e72b9cb1f83ba677e97909
Arman Aminian <arman.aminian78@gmail.com>
2023-04-19 02:02:22 +0330

    feat: text preprocessing added and dvc reproduced.

--- src/feature_eng/manual_features.py
+++ src/feature_eng/manual_features.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+from hazm import Normalizer, Stemmer, Lemmatizer, WordTokenizer
+import emoji
 
 
 def make_model_int(model):
@@ -9,17 +11,34 @@ def make_model_int(model):
         return 1369
 
 
-def manual_feature_eng(train_test_dir, out_dir):
-    os.makedirs(out_dir, exist_ok=True)
+def preprocess_description(text, tokenizer, normalizer, lemmatizer):
 
+    # remove emojies
+    text = emoji.get_emoji_regexp().sub(r'', text)
+    # tokenize - replace link-number-
+    words = tokenizer.tokenize(' '.join(tokenizer.tokenize(text)))
+    # replace ي with ی and remove arabic symbols
+    words = [normalizer.character_refinement(word) for word in words]
+
+    text = ' '.join([lemmatizer.lemmatize(word) for word in words])
+    return text
+
+
+def manual_feature_eng(train_test_dir, out_dir):
+    tokenizer = WordTokenizer(join_verb_parts=False, replace_links=True, replace_numbers=True)
+    normalizer = Normalizer()
+    lemmatizer = Lemmatizer()
+
+    os.makedirs(out_dir, exist_ok=True)
     for data_type in ['train', 'test', 'val']:
         # read file
         data = pd.read_csv(os.path.join(train_test_dir, f'x_{data_type}.csv'),
                            index_col=0)
 
         data.model = data.model.apply(make_model_int)
-        data['age'] = data.model.apply(lambda x: 1402-x)
+        data['age'] = data.model.apply(lambda x: 1402 - x)
         data.drop(columns='model')
         data['price_per_meter'] = data['price'] / data['size']
+        data['desc'] = data.desc.apply(lambda x: preprocess_description(x, tokenizer, normalizer, lemmatizer))
 
         data.to_csv(os.path.join(out_dir, f'x_{data_type}.csv'))
