c48211619fd86c31122e4c8625ca829a30209c9e
Arman Aminian <arman.aminian78@gmail.com>
2023-04-19 01:38:14 +0330

    feat: manual feature eng added.

---
+++ src/feature_eng/manual_features.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+
+def make_model_int(model):
+    try:
+        return int(model)
+    except:
+        return 1369
+
+
+def manual_feature_eng(train_test_dir, out_dir):
+
+    for data_type in ['train', 'test', 'val']:
+        # read file
+        data = pd.read_csv(os.path.join(train_test_dir, f'x_{data_type}.csv'),
+                           index_col=0)
+
+        data.model = data.model.apply(make_model_int)
+        data['age'] = data.model.apply(lambda x: 1402-x)
+        data.drop(columns='model')
+        data['price_per_meter'] = data['price'] / data['size']
+
+        data.to_csv(os.path.join(out_dir, f'x_{data_type}.csv'))
